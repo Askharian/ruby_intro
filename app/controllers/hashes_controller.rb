2ca2daca858fcf9aa6400d5c9ab2c200e4182d97
Hugh Berglind <hugh.berglind@gmail.com>
2015-09-23 10:28:49 -0500

    finished array problems

--- app/controllers/hashes_controller.rb
+++ app/controllers/hashes_controller.rb
@@ -38,7 +38,7 @@ class HashesController < ApplicationController
 
     address = { "correct" => "Sears", "incorrect" => "Willis"}
 
-    @result = "replace this string with your answer"
+    @result = address["correct"]
     render 'hashes'
   end
 
@@ -73,7 +73,7 @@ class HashesController < ApplicationController
                     }
                   }
 
-    @result = "replace this string with your answer"
+    @result = attraction["address"]["street"]
     render 'hashes'
   end
 
@@ -100,6 +100,7 @@ class HashesController < ApplicationController
     #============================================
 
     attraction = { "first_name" => "Sears"}
+    attraction["last_name"]= "Tower"
 
     @result = "#{attraction['first_name']} #{attraction['last_name']}"
     render "hashes"
